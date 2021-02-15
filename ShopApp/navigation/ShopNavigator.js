dc2602b121f78500a46f6759e95280a50925d35e
Zihan Zhang (Steven) <zihanzhang0116@gmail.com>
2021-02-16 01:24:15 +0800

    Add files via upload

---
+++ ShopApp/navigation/ShopNavigator.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { DrawerNavigatorItems } from 'react-navigation-drawer';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { Platform, SafeAreaView, Button, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useDispatch } from 'react-redux';
+
+import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import CartScreen from '../screens/shop/CartScreen';
+import OrdersScreen from '../screens/shop/OrdersScreen';
+import UserProductsScreen from '../screens/user/UserProductsScreen';
+import EditProductScreen from '../screens/user/EditProductScreen';
+import AuthScreen from '../screens/user/AuthScreen';
+import StartupScreen from '../screens/StartupScreen';
+import Colors from '../constants/Colors';
+import * as authActions from '../store/actions/auth';
+
+const defaultNavOptions = {
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+    },
+    headerTitleStyle: {
+        fontFamily: 'open-sans-bold'
+    },
+    headerBackTitleStyle: {
+        fontFamily: 'open-sans'
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+};
+
+const ProductsNavigator = createStackNavigator(
+    {
+        ProductsOverview: ProductsOverviewScreen,
+        ProductDetail: ProductDetailScreen,
+        Cart: CartScreen
+    },
+    {
+        navigationOptions: {
+            drawerIcon: drawerConfig => (
+                <Ionicons
+                name={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+                size={23}
+                color={drawerConfig.tintColor}
+                />
+            )
+        },
+        defaultNavigationOptions: defaultNavOptions
+    }
+);
+
+const OrdersNavigator = createStackNavigator(
+    {
+        Orders: OrdersScreen
+    },
+    {
+        navigationOptions: {
+            drawerIcon: drawerConfig => (
+                <Ionicons
+                name={Platform.OS === 'android' ? 'md-list' : 'ios-list'}
+                size={23}
+                color={drawerConfig.tintColor}
+                />
+            )
+        },
+        defaultNavigationOptions: defaultNavOptions
+    }
+);
+
+const AdminNavigator = createStackNavigator(
+    {
+        UserProducts: UserProductsScreen,
+        EditProduct: EditProductScreen
+    },
+    {
+        navigationOptions: {
+            drawerIcon: drawerConfig => (
+                <Ionicons
+                name={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+                size={23}
+                color={drawerConfig.tintColor}
+                />
+            )
+        },
+        defaultNavigationOptions: defaultNavOptions
+    }
+);
+
+const ShopNavigator = createDrawerNavigator(
+    {
+        Products: ProductsNavigator,
+        Orders: OrdersNavigator,
+        Admin: AdminNavigator
+    },
+    {
+        contentOptions: {
+            activeTintColor: Colors.primary
+        },
+        contentComponent: props => {
+            const dispatch = useDispatch();
+            return (
+                <View style = {{
+                    flex: 1,
+                    padding: 20
+                }}>
+                <SafeAreaView forceInset = {{
+                    top: 'always',
+                    horizontal: 'never'
+                }}>
+                  <DrawerNavigatorItems {...props}/>
+                  <Button title = 'Logout' color = {Colors.primary} onPress = {() => {
+                    dispatch(authActions.logout());
+                // props.navigation.navigate('Auth');
+                }}/>
+                </SafeAreaView>
+            </View>);
+        }
+    }
+);
+
+const AuthNavigator = createStackNavigator(
+    {
+        Auth: AuthScreen
+    },
+    {
+        defaultNavigationOptions: defaultNavOptions
+    }
+);
+
+const MainNavigator = createSwitchNavigator({
+    Startup: StartupScreen,
+    Auth: AuthNavigator,
+    Shop: ShopNavigator
+});
+
+export default createAppContainer(MainNavigator);
---
+++ ShopApp/store/actions/auth.js
@@ -0,0 +1,139 @@
+import { AsyncStorage } from 'react-native';
+
+// export const SIGNUP = 'SIGNUP';
+// export const LOGIN = 'LOGIN';
+export const AUTHENTICATE = 'AUTHENTICATE';
+export const LOGOUT = 'LOGOUT';
+
+let timer;
+
+export const authenticate = (userId, token, expiryDate) => {
+    return dispatch => {
+        dispatch(setLogoutTimer(expiryDate));
+        dispatch({
+            type: AUTHENTICATE,
+            userId: userId,
+            token: token
+        });
+    }
+};
+
+export const signup = (email, password) => {
+    return async dispatch => {
+        const response = await fetch(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAqoH9bLo2OUMRSTyttWmh4sKjTQ4BPVB8',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                    returnSecureToken: true
+                })
+            }
+        );
+
+        if (!response.ok) {
+            const errorResData = await response.json();
+            const errorId = errorResData.error.message;
+            let message = 'Something went wrong!';
+            if (errorId === 'EMAIL_EXISTS') {
+                message = 'This email exists already!';
+            }
+            throw new Error(message);
+        }
+
+        const resData = await response.json();
+        console.log(resData);
+        dispatch(authenticate(resData.localId, resData.idToken, parseInt(resData.expiresIn) * 1000));
+        const expirationDate = new Date(
+            new Date().getTime() + parseInt(resData.expiresIn) * 1000
+        );
+        saveDataToStorage(resData.idToken, resData.localId, expirationDate);
+    };
+};
+
+export const login = (email, password) => {
+    return async dispatch => {
+        const response = await fetch(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAqoH9bLo2OUMRSTyttWmh4sKjTQ4BPVB8',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                    returnSecureToken: true
+                })
+            }
+        );
+
+        if (!response.ok) {
+            const errorResData = await response.json();
+            const errorId = errorResData.error.message;
+            // console.log(errorId);
+            let message = 'Something went wrong!';
+            if (errorId === 'EMAIL_NOT_FOUND') {
+                message = 'This email could not be found!';
+            } else if (errorId === 'INVALID_PASSWORD') {
+                message = 'This password is not valid!';
+            } else if (errorId === 'MISSING_PASSWORD') {
+                message = 'Please re-enter your password!';
+            }
+            throw new Error(message);
+        }
+
+        const resData = await response.json();
+        console.log(resData);
+        dispatch(authenticate(resData.localId, resData.idToken, parseInt(resData.expiresIn) * 1000));
+        const expirationDate = new Date(
+            new Date().getTime() + parseInt(resData.expiresIn) * 1000
+        );
+        saveDataToStorage(resData.idToken, resData.localId, expirationDate);
+    };
+};
+
+export const logout = () => {
+    clearLogoutTimer();
+    AsyncStorage.removeItem('userData');
+    return {
+        type: LOGOUT
+    };
+};
+
+const clearLogoutTimer = () => {
+    if (timer) {
+        clearTimeout(timer);
+    }
+};
+
+const setLogoutTimer = expirationTime => {
+    return dispatch => {
+        timer = setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime);
+    };
+};
+
+const saveDataToStorage = (token, userId, expirationDate) => {
+    AsyncStorage.setItem(
+        'userData',
+        JSON.stringify({
+            token: token,
+            userId: userId,
+            expiryDate: expirationDate.toISOString()
+        })
+    );
+};
+
+
+
+
+
+
+
+
